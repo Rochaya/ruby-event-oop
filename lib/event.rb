120341d7d536fae3773508815dd5838f364b373c
Rochaya <rochaya@DESKTOP-OCT9BF6.localdomain>
2023-04-24 20:21:13 +0200

    pospone ok voir version ruby

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,27 @@
-class Event
+require 'pry'
+require 'date'
 
+class Event
+    
+    attr_accessor :start_date
+    attr_accessor :duration
+    attr_accessor :title
+    attr_accessor :attendees
+
+    @@all_events = Array.new
+
+    def initialize(start_date, duration, title, attendees)
+        @start_date = DateTime.strptime(start_date, "%Y-%m-%d %H:%M")
+        @duration = duration * 60
+        @title = title
+        @attendees = [attendees]
+        @@all_events.push(self)
+    end
+
+    def postpone
+        @start_date += (1440*60)
+        puts "L'evenement a ete reporter de 24h, soit prévu pour le #{@date_and_hour.day}/#{@date_and_hour.month}/#{@date_and_hour.year} prochain à #{@date_and_hour.hour}h#{@date_and_hour.min}."
+    end
 end
+
+binding.pry
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,20 @@
+require 'pry'
 class User
+    attr_accessor :email
+    attr_accessor :age
+    @@all_users = Array.new
 
+    def initialize(email_to_save, age_to_save)
+        @email = email_to_save
+        @age = age_to_save
+        @@all_users.push(self)
+    end
+
+    def self.all
+        return @@all_users
+    end
 end
+
+binding.pry
+#julie = User.new("julie@gmail.com", 32)
+
